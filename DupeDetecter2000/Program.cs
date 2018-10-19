24f1a71c066ad76802411797d6eb66b6ebf9e927
Mikael Rasmussen <mikael@rubixy.com>
2018-10-19 16:39:08 +0200

    Add project files.

---
+++ DupeDetecter2000/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DupeDetecter2000
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
